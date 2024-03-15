a21d7e9ae7ecef5c615e4a26f773e73ac9ba1223
ShendeManas <132647563+ShendeManas@users.noreply.github.com>
2024-03-15 21:35:41 +0530

    Update std.js [Adding Search function which will look for the particular record

--- Files/std.js
+++ Files/std.js
@@ -118,5 +118,30 @@ function showTable(){
     })
 }
 
+function search(){
+  var input, filter, table, tr, td, i, txtValue,txtValue1,txtValue2;
+  input = document.getElementById("search");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tbody");
+  tr = table.getElementsByTagName("tr");
+
+
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    td1 = tr[i].getElementsByTagName("td")[2];
+    td2 = tr[i].getElementsByTagName("td")[5];
+    if (td || td1 || td2) {
+      txtValue = td.textContent || td.innerText;
+      txtValue1 = td1.textContent || td1.innerText;
+      txtValue2 = td2.textContent || td2.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1 || txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 
 
